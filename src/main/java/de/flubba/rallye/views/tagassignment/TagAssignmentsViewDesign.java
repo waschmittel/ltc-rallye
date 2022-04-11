0a4ccaa53a0fe31daae8cd800bd9f366d4152ddf
Daniel Flassak <daniel.flassak@dm.de>
2022-04-11 21:01:04 +0200

    set a correct duration and remove unnecessary component alignment

--- src/main/java/de/flubba/rallye/views/tagassignment/TagAssignmentsViewDesign.java
+++ src/main/java/de/flubba/rallye/views/tagassignment/TagAssignmentsViewDesign.java
@@ -7,14 +7,12 @@ import de.flubba.rallye.entity.TagAssignment;
 import javax.annotation.PostConstruct;
 
 import static com.vaadin.flow.component.grid.Grid.SelectionMode.NONE;
-import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 
 abstract class TagAssignmentsViewDesign extends VerticalLayout {
     final Grid<TagAssignment> tagAssignments = new Grid<>(TagAssignment.class);
 
     @PostConstruct
     private void init() {
-        setDefaultHorizontalComponentAlignment(CENTER); //TODO: check if this works
         tagAssignments.removeColumn(tagAssignments.getColumnByKey("id"));
         tagAssignments.setSelectionMode(NONE);
         addAndExpand(tagAssignments);
