a27312ed0d6f6d0fdb8099686c8ee09087f882b6
Daniel Flassak <daniel.flassak@dm.de>
2022-05-08 22:57:28 +0200

    remove old, obsolete example things

--- src/main/java/de/flubba/rallye/data/service/SamplePersonRepository.java
+++
@@ -1,9 +0,0 @@
-package de.flubba.rallye.data.service;
-
-import de.flubba.rallye.data.entity.SamplePerson;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
-
-}
