9f6a760641756d3d76b7b043ce2d75ce2a364aec
Daniel Flassak <daniel.flassak@dm.de>
2024-03-17 02:09:20 +0100

    use mDNS

---
+++ src/main/java/de/flubba/rallye/rest/MDNSAdvertiser.java
@@ -0,0 +1,34 @@
+package de.flubba.rallye.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class MDNSAdvertiser implements AutoCloseable {
+    private final JmDNS jmDNS;
+
+    public MDNSAdvertiser(@Value("${server.port}") int port) {
+        try {
+            jmDNS = JmDNS.create(null, null, 200);
+
+            // Register a service
+            ServiceInfo serviceInfo = ServiceInfo.create("_ltc-rallye._tcp.local.", "There can only be one!", port, "");
+            jmDNS.registerService(serviceInfo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        log.info("closing jmDNS");
+        jmDNS.close();
+    }
+}
